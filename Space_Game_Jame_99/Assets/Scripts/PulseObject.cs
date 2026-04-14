a05a11083033013be87d40c3f81510999c6ed055
Yatoujourbezoind1psedo <bonath03@gmail.com>
2026-04-14 15:59:10 +0200

    Ajout d'un visuel en rythme + spawn fait apparaitre uen ligne - 1 météor

---
+++ Space_Game_Jame_99/Assets/Scripts/PulseObject.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PulseObject : MonoBehaviour
+{
+    [SerializeField] float _pulseSize = 1.15f; 
+    [SerializeField] float _returnSpeed = 5f; 
+    private Vector3 _startSize ; 
+
+    private void Start()
+    {
+        _startSize = transform.localScale; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = Vector3.Lerp(transform.localScale, _startSize, Time.deltaTime * _returnSpeed);
+    }
+
+    public void Pulse()
+    {
+        transform.localScale = _startSize * _pulseSize; 
+    }
+}
--- Space_Game_Jame_99/Assets/Scripts/SpawnManagement.cs
+++ Space_Game_Jame_99/Assets/Scripts/SpawnManagement.cs
@@ -5,8 +5,8 @@ public class SpawnManagement : MonoBehaviour
 {
     [SerializeField] private GameObject[] meteors; 
 
-    [SerializeField] private Transform chemin1, chemin2; 
-    private Transform[] pointsCh1, pointsCh2; //Listes des points des chemins 
+    [SerializeField] private Transform chemin1, chemin2, chemin3, chemin4; 
+    private Transform[] pointsCh1, pointsCh2, pointsCh3, pointsCh4; //Listes des points des chemins 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,9 +25,19 @@ public class SpawnManagement : MonoBehaviour
             pointsCh2[i] = chemin2.GetChild(i); 
         }
 
+        pointsCh3 = new Transform[chemin3.childCount]; 
+        for(int i = 0; i < chemin2.childCount; i++)
+        {
+            pointsCh3[i] = chemin3.GetChild(i); 
+        }
 
-        SpawnMeteor(0, pointsCh1); 
-        SpawnMeteor(1, pointsCh2); 
+        pointsCh4 = new Transform[chemin4.childCount]; 
+        for(int i = 0; i < chemin2.childCount; i++)
+        {
+            pointsCh4[i] = chemin4.GetChild(i); 
+        }
+
+        SpawnMeteorsExceptChemin(0); 
     }
 
     // Update is called once per frame
@@ -35,6 +45,7 @@ public class SpawnManagement : MonoBehaviour
     {
     }
 
+/*
     public void SpawnMeteor1()
     {
         SpawnMeteor(0, pointsCh1); 
@@ -43,13 +54,48 @@ public class SpawnManagement : MonoBehaviour
     public void SpawnMeteor2()
     {
         SpawnMeteor(1, pointsCh2); 
-    }
+    }*/
 
     public void SpawnMeteor(int meteorX, Transform[] pointsChX)
     {
         GameObject mete = Instantiate(meteors[meteorX], pointsChX[0].transform.position, meteors[meteorX].transform.rotation);  //Instantie le météore X dnas la lsite au point 0 du chemin donné
         FollowPath sc = mete.GetComponent<FollowPath>(); // Récupère le script Follow Path
         sc.points = pointsChX; //Lui donne les points qu'il doit suivre
+    }
+
+    public void SpawnMeteorsExceptChemin (int oubli) //fait apparaitre météore sauf à l'emplacement dnné (0 = pas de météores); 
+    {
+        switch (oubli)
+        {
+            case 0:
+                break; 
+
+            case 1: //Donc on ne prend pas le premier météore 
+                SpawnMeteor(0, pointsCh2); 
+                SpawnMeteor(1, pointsCh3);
+                SpawnMeteor(0, pointsCh4);
+                break; 
+
+            case 2: //ici pas le deuxième et bla bla bla
+                SpawnMeteor(0, pointsCh1); 
+                SpawnMeteor(1, pointsCh3);
+                SpawnMeteor(0, pointsCh4);
+                break; 
+
+            case 3: 
+                SpawnMeteor(0, pointsCh1); 
+                SpawnMeteor(1, pointsCh2);
+                SpawnMeteor(0, pointsCh4);
+                break;
+
+
+            case 4: 
+                SpawnMeteor(0, pointsCh1); 
+                SpawnMeteor(1, pointsCh2);
+                SpawnMeteor(0, pointsCh3);
+                break;
+
+        }
     }
 
 }
