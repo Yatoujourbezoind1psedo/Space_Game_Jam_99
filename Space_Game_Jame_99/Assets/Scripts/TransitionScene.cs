e0507a8e4b88c5a379720b92961c48cb7fc3ea48
Yatoujourbezoind1psedo <bonath03@gmail.com>
2026-04-16 22:39:22 +0200

    ajout d'un comportement pour changer de scène (crédits, play et pt options) + quit app

---
+++ Space_Game_Jame_99/Assets/Scripts/TransitionScene.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionScene : MonoBehaviour
+{
+    public void GoToScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+        Time.timeScale = 1f;//Je le rajoute parce que sinon ça reste à 0 en faisant pause (juste au cas où)
+    }
+    public void QuitApp() { //attention quit marche qeu en build
+        Application.Quit();
+        Debug.Log("Application has quit");
+    }
+}
