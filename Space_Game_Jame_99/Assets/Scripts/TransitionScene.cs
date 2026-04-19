3cfb6fd29d6f57543fbf7995fc06950bd7631f80
Yatoujourbezoind1psedo <bonath03@gmail.com>
2026-04-19 18:32:33 +0200

    ajout clicaction (pas fini)

--- Space_Game_Jame_99/Assets/Scripts/TransitionScene.cs
+++ Space_Game_Jame_99/Assets/Scripts/TransitionScene.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement; // Obligatoire pour gérer les scènes
 
 public class SceneChanger : MonoBehaviour
 {
+    private string test; 
     public void ChargerScene(string Sana test)
     {
         SceneManager.LoadScene(Sana test);
