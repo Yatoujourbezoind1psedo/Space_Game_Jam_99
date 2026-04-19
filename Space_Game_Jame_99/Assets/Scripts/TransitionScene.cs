10696584c5d151fc5a73c0a239cfe945aff2bc51
Cunning Turtle <150448697+cunningturtle@users.noreply.github.com>
2026-04-19 18:30:49 +0200

    bjbj

--- Space_Game_Jame_99/Assets/Scripts/TransitionScene.cs
+++ Space_Game_Jame_99/Assets/Scripts/TransitionScene.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine;
-using UnityEngine.SceneManagement; // Obligatoire pour gérer les scènes
 
-public class SceneChanger : MonoBehaviour
-{
-    public void ChargerScene(string Sana test)
-    {
-        SceneManager.LoadScene(Sana test);
+public class TransitionScene : MonoBehaviour
+{ 
+    public void GoToScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+        Time.timeScale = 1f;//Je le rajoute parce que sinon ça reste à 0 en faisant pause (juste au cas où)
+    }
+    public void QuitApp() { //attention quit marche qeu en build
+        Application.Quit();
+        Debug.Log("Application has quit");
     }
 }
